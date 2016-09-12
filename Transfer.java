7e29620b75acdc7b37647a8234570662ed903ec0
itchy-al <scarletammo9115@yahoo.co.jp>
2016-09-12 17:13:44 +0900

    I hope commit...

--- Transfer.java
+++ Transfer.java
@@ -1,21 +1,17 @@
 import java.util.ArrayList;
-import java.net.socket;
 
 public class Transfer {
+
 	/**
 	 * 配列っぽいけど配列ではなく、Ｌｉｓｔを使ってください。
 	 * お願いします。
 	 */
-	private String recieveData;
+	private String[] recieveData;
 
 	private String[] submitData;
 
 	private Socket socket;
 
-	public Transfer(Socket socket){
-		
-	}
-	
 	public void recieve(String message) {
 
 	}
